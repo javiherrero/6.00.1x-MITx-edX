5d4aef3912b0f73198d163625505b95f87477dc0
Javier Herrero <javiherar@yahoo.com>
2015-02-10 17:07:45 +0100

    LEC 7 - Problem 7

---
+++ lecture7/lec7_problem7.py
@@ -0,0 +1,18 @@
+# Desc: Lec 7, Problem 7.
+# Author: Javier Herrero Arnanz.
+
+def rem(x, a):
+    """
+    x: a non-negative integer argument
+    a: a positive integer argument
+
+    returns: integer, the remainder when x is divided by a.
+    """
+    if x == a:
+        return 0
+    elif x < a:
+        return x
+    else:
+        return rem(x-a, a)
+        
+print rem(12,5)
