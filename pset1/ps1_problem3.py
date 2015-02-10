f7fa69bdc9e9d5c92a3458869025e19338ac6567
Javier Herrero <javiherar@yahoo.com>
2015-02-10 14:15:38 +0100

    PSET 1 - Problem 3 - Alphabetical Substrings

---
+++ pset1/ps1_problem3.py
@@ -0,0 +1,19 @@
+# Desc: Problem Set 1 - Problem 3 - Alphabetical Substrings
+# Author: Javier Herrero Arnanz.
+
+# String to evaluate.
+s = 'azcbobobegghakl'
+
+# Search the longest alphabetical substring.
+alphStr = ''
+currentStr = ''
+for i in range(len(s)):
+        currentStr += s[i]
+        if ((i == len(s)-1) or (s[i] > s[i + 1])):
+            # Check alphabetical substring.
+            if (len(currentStr) > len(alphStr)):
+                alphStr = currentStr
+            currentStr = ''            
+
+# Print the result.       
+print ('Longest substring in alphabetical order is: ' + alphStr)
