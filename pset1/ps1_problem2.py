03c01b21059a6d819cad44ce6e84a24f4289f6af
Javier Herrero <javiherar@yahoo.com>
2015-02-10 14:13:26 +0100

    Create ps1_problem2.py

---
+++ pset1/ps1_problem2.py
@@ -0,0 +1,14 @@
+# Desc: Problem Set 1 - Problem 2 - Counting Bobs
+# Author: Javier Herrero Arnanz.
+
+# String to evaluate.
+s = 'azcbobobegghakl'
+
+# Replace b -> bb
+s = s.replace('b','bb')
+
+# Count the bobs.
+nBobs = s.count('bob',0,len(s))
+
+# Print the result.    
+print ('Number of times bob occurs is: ' + str(nBobs))
