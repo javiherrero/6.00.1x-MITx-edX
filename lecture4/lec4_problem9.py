f99cc0677436c1a4c47e5e7c5f699f811f0a5cde
Javier Herrero <javiherar@yahoo.com>
2015-02-10 16:52:53 +0100

    LEC 4 - Problem 9

---
+++ lecture4/lec4_problem9.py
@@ -0,0 +1,12 @@
+# Desc: Lec 4, Problem 9.
+# Author: Javier Herrero Arnanz.
+
+def odd(x):
+    '''
+    x: int or float.
+
+    returns: True if x is odd, False otherwise
+    '''
+    return ((x % 2) != 0)
+    
+print odd(223)
