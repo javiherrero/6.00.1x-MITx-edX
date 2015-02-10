9bca20520241119e3cda5b40dbf0c426cd9c8107
Javier Herrero <javiherar@yahoo.com>
2015-02-10 16:17:52 +0100

    Quiz - Problem 8-3

---
+++ quiz/quiz_problem8_3.py
@@ -0,0 +1,10 @@
+# Desc: Quiz - Problem 8-3.
+# Author: Javier Herrero Arnanz.
+
+def babylon(a):
+    def test(x):
+        return 0.5 * ((a / x) + x)
+    return test
+
+def sqrt(a):
+    return fixedPoint(babylon(a), 0.0001)
