73d03ecf4e99a9adb9c8c114c14eee007b0fb391
Javier Herrero <javiherar@yahoo.com>
2015-02-10 16:48:00 +0100

    LEC 4 - Problem 4

---
+++ lecture4/lec4_problem4.py
@@ -0,0 +1,11 @@
+# Desc: Lec 4, Problem 4.
+# Author: Javier Herrero Arnanz.
+
+def evalQuadratic(a, b, c, x):
+    '''
+    a, b, c: numerical values for the coefficients of a quadratic equation
+    x: numerical value at which to evaluate the quadratic.
+    '''
+    return (a*x**2 + b*x + c)
+    
+print evalQuadratic(1, 2, 3, 4)
