9b978b3d6a854bbdd8dd13b13bdd4eac0f6502f1
Javier Herrero <javiherar@yahoo.com>
2015-02-25 23:26:55 +0100

    P

--- pset6/ps6_encryption.py
+++ pset6/ps6_encryption.py
@@ -156,18 +156,18 @@ def findBestShift(wordList, text):
     
     # For each possible shift from 0 to 26.
     for shift in range(26):
-	    # Shift the entire text and split the text up into a list of the individual words.
-	    sText = applyShift(text, shift).split(' ')
-	    # Count the number of valid words in this list.
-	    countRW = 0
-	    for word in sText:
-	      if (isWord(wordList, word)):
-	        countRW += 1
-	    # If the number of valid words is more than the largest number of real words found:
-	    if (countRW > maxRealW):
-		    # Record the number of valid words and set the best shift to the current shift.
-		    maxRealW = countRW
-		    bestShift = shift
+    	# Shift the entire text and split the text up into a list of the individual words.
+    	sText = applyShift(text, shift).split(' ')
+    	# Count the number of valid words in this list.
+    	countRW = 0
+    	for word in sText:
+    		if (isWord(wordList, word)):
+    			countRW += 1
+    	# If the number of valid words is more than the largest number of real words found:
+    	if (countRW > maxRealW):
+    		# Record the number of valid words and set the best shift to the current shift.
+	    	maxRealW = countRW
+	    	bestShift = shift
 
     # Return the best shift.
     return bestShift
